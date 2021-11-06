dd6cceef0aac39acb84365dad3b9f1f866adf7a0
sabandurna <sabandurna@gmail.com>
2021-11-06 15:35:55 +0100

    [TUP-13] chor: create web api project along with the test project

---
+++ src/api/CustomerAPI/HG.Tuplify.Customer.WebApi.Tests/ValuesControllerTests.cs
@@ -0,0 +1,33 @@
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+using Amazon.Lambda.TestUtilities;
+using Amazon.Lambda.APIGatewayEvents;
+using Newtonsoft.Json;
+
+
+namespace HG.Tuplify.Customer.WebApi.Tests
+{
+    public class ValuesControllerTests
+    {
+
+
+        [Fact]
+        public async Task TestGet()
+        {
+            var lambdaFunction = new LambdaEntryPoint();
+
+            var requestStr = await File.ReadAllTextAsync("./SampleRequests/ValuesController-Get.json");
+            var request = JsonConvert.DeserializeObject<APIGatewayProxyRequest>(requestStr);
+            var context = new TestLambdaContext();
+            var response = await lambdaFunction.FunctionHandlerAsync(request, context);
+
+            Assert.Equal(200, response.StatusCode);
+            Assert.Equal("[\"value1\",\"value2\"]", response.Body);
+            Assert.True(response.MultiValueHeaders.ContainsKey("Content-Type"));
+            Assert.Equal("application/json; charset=utf-8", response.MultiValueHeaders["Content-Type"][0]);
+        }
+
+
+    }
+}
